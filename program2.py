14c090b7bc61b3facc57bf0a5e167e158b04012a
Vishnuchatharaju <148743763+Vishnuchatharaju@users.noreply.github.com>
2023-10-29 15:13:14 +0530

    added new python program

---
+++ program2.py
@@ -0,0 +1,61 @@
+# print("Hello!")
+# name = input("Name: ")
+# print("Hello, " + name)
+# n = int(input("Number: "))
+# if n > 0:
+#     print("n is positive number")
+# elif n < 0:
+#     print("n is negative number")
+# else:
+#    { print("n is zero")}
+# names = ["hi" , "ron", "don", "lon"]
+# print(names[0])
+# names.append("told")
+# print(names)
+# names.sort()
+# print(names)
+# s= set()
+# s.add(1)
+# s.add(2)
+# s.add(3)
+# s.add(5)
+# s.add(4)
+# s.remove(2)
+# print(s)
+# print(len(s))
+# print(f"The set has {s} elements")
+# for i in [0,1,2,3,4,5,6]:
+#     print(i) 
+#     # or
+# for name in names:
+#     print(name)
+# t = "vishnu"
+# for character in t:
+#         print(character)
+#  // dictinories
+houses = {"red":"ravi","green":"ramu"}
+houses["blue"] = "ravi"
+print(houses["red"])-
+print(houses["blue"])
+print(houses["green"])
+from program3 import square
+for i in range(10):
+    print(f"The square of {i} is {square(i)}")    
+    # //or  
+import program3
+for i in range(10):
+ print(f"The square of {i} is {program3.square(i)}")    
+ class Point:
+   def __init__(self, input1, input2):
+        self.x = input1
+        self.y = input2
+p = Point(2, 6)
+print(p.x)
+print(p.y)
+class Point:
+    def __init__(self, input1, input2):
+        self.x = input1
+        self.y = input2
+p = Point(2, 6)
+print(p.x)
+print(p.y)
